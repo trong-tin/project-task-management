e0b893071e2d9df8df308dc2713c02f99282137f
trong-tin <trongtinvlyk@gmail.com>
2024-06-04 23:44:13 +0700

    Tính năng tạo mới công việc và thay đổi 1 lúc nhiều trạng thái

--- api/v1/controllers/taskController.js
+++ api/v1/controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require("../models/taskModel");
 const paginationHelper = require("../../../helpers/paginationHelper");
 const searchHelper = require("../../../helpers/searchHelper");
+
 //[GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
   try {
@@ -84,6 +85,58 @@ module.exports.changeStatus = async (req, res) => {
     res.json({
       code: 400,
       message: "Cập nhật trạng thái không thành công",
+    });
+  }
+};
+
+//[PATCH] /api/v1/tasks/change-multi
+module.exports.changeMulti = async (req, res) => {
+  try {
+    const { ids, key, value } = req.body;
+    switch (key) {
+      case "status":
+        await Task.updateMany(
+          {
+            _id: { $in: ids },
+          },
+          {
+            status: value,
+          }
+        );
+        res.json({
+          code: 200,
+          message: "Cập nhật trạng thái thành công",
+        });
+        break;
+      default:
+        res.json({
+          code: 400,
+          message: "Không tồn tại",
+        });
+        break;
+    }
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại",
+    });
+  }
+};
+
+//[POST] /api/v1/tasks/create
+module.exports.create = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const data = await task.save();
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công",
+      data: data,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại",
     });
   }
 };
