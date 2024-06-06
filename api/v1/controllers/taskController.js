f1598cf03efd561dc4d52f467d1c0aef386bac13
trong-tin <trongtinvlyk@gmail.com>
2024-06-06 13:24:02 +0700

    Xóa công việc

--- api/v1/controllers/taskController.js
+++ api/v1/controllers/taskController.js
@@ -108,6 +108,21 @@ module.exports.changeMulti = async (req, res) => {
           message: "Cập nhật trạng thái thành công",
         });
         break;
+      case "delete":
+        await Task.updateMany(
+          {
+            _id: { $in: ids },
+          },
+          {
+            deleted: true,
+            deletedAt: new Date(),
+          }
+        );
+        res.json({
+          code: 200,
+          message: "Xóa các công việc thành công",
+        });
+        break;
       default:
         res.json({
           code: 400,
@@ -156,6 +171,23 @@ module.exports.edit = async (req, res) => {
     res.json({
       code: 400,
       message: "Lỗi",
+    });
+  }
+};
+
+//[DELETE] /api/v1/tasks/delete/:id
+module.exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Task.updateOne({ _id: id }, { deleted: true, deletedAt: new Date() });
+    res.json({
+      code: 200,
+      message: "Xóa thành công",
+    });
+  } catch {
+    res.json({
+      code: 400,
+      message: "Xóa không thành công",
     });
   }
 };
--- api/v1/routes/taskRoute.js
+++ api/v1/routes/taskRoute.js
@@ -9,4 +9,5 @@ router.patch("/change-status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.post("/create", controller.create);
 router.patch("/edit/:id", controller.edit);
+router.delete("/delete/:id", controller.delete);
 module.exports = router;
