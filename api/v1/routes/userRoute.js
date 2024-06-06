35f37b8df65a14ec945a36a60c1ea233e9e20e0e
trong-tin <trongtinvlyk@gmail.com>
2024-06-06 19:57:29 +0700

    Tính năng SHOW trang cá nhân

--- api/v1/routes/userRoute.js
+++ api/v1/routes/userRoute.js
@@ -7,4 +7,5 @@ router.post("/login", controller.login);
 router.post("/password/forgot", controller.forgotPassword);
 router.post("/password/otp", controller.otpPassword);
 router.post("/password/reset", controller.resetPassword);
+router.get("/detail", controller.detail);
 module.exports = router;
