6989348cdb5fbf58911c0787b487d95324fddbf7
trong-tin <trongtinvlyk@gmail.com>
2024-06-06 18:15:16 +0700

    Tính năng đăng nhập

--- api/v1/controllers/userController.js
+++ api/v1/controllers/userController.js
@@ -3,29 +3,71 @@ const User = require("../models/userModel");
 
 // [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
-  req.body.password = md5(req.body.password);
-  const existEmail = await User.findOne({
-    email: req.body.email,
-    deleted: false,
-  });
-  if (existEmail) {
+  try {
+    req.body.password = md5(req.body.password);
+    const existEmail = await User.findOne({
+      email: req.body.email,
+      deleted: false,
+    });
+    if (existEmail) {
+      res.json({
+        code: 400,
+        message: "Email đã tồn tại",
+      });
+    } else {
+      const user = new User({
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      user.save();
+      const token = user.token;
+      res.cookie("token", token);
+      res.json({
+        code: 200,
+        message: "Đăng kí thành công",
+        token: token,
+      });
+    }
+  } catch {
     res.json({
       code: 400,
-      message: "Email đã tồn tại",
+      message: "Lỗi 404",
     });
-  } else {
-    const user = new User({
-      fullName: req.body.fullName,
+  }
+};
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  try {
+    const existEmail = await User.findOne({
       email: req.body.email,
-      password: req.body.password,
+      deleted: false,
     });
-    user.save();
-    const token = user.token;
-    res.cookie("token", token);
+    if (existEmail) {
+      req.body.password = md5(req.body.password);
+      if (req.body.password === existEmail.password) {
+        res.json({
+          code: 200,
+          message: "Đăng nhập thành công",
+          token: existEmail.token,
+        });
+      } else {
+        res.json({
+          code: 400,
+          message: "Sai mật khẩu",
+        });
+      }
+    } else {
+      res.json({
+        code: 400,
+        message: "Email đăng nhập không đúng",
+      });
+    }
+  } catch {
     res.json({
-      code: 200,
-      message: "Đăng kí thành công",
-      token: token,
+      code: 400,
+      message: "Lỗi 404",
     });
   }
 };
--- api/v1/routes/userRoute.js
+++ api/v1/routes/userRoute.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const controller = require("../controllers/userController");
 
 router.post("/register", controller.register);
+router.post("/login", controller.login);
 module.exports = router;
